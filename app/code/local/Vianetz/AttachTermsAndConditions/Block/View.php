3e40d2a1aa4ac7aafd27ef5007b6b6ebce3448fd
Christoph Massmann <c.massmann@vianetz.com>
2016-02-28 12:52:44 +0100

    reformatted some things

--- app/code/local/Vianetz/AttachTermsAndConditions/Block/View.php
+++ app/code/local/Vianetz/AttachTermsAndConditions/Block/View.php
@@ -1,23 +1,22 @@
 <?php
+
 class Vianetz_AttachTermsAndConditions_Block_View extends Mage_Core_Block_Template
 {
-	protected function _toHtml() {
-            $agreements = Mage::getModel('checkout/agreement')->getCollection()
-                    ->addStoreFilter(Mage::app()->getStore()->getId())
-                    ->addFieldToFilter('is_active', 1)
-                    ->addFieldToFilter('name', $this->getBlockId());
+    protected function _toHtml()
+    {
+        $agreements = Mage::getModel('checkout/agreement')
+            ->getCollection()
+            ->addStoreFilter(Mage::app()->getStore()->getId())
+            ->addFieldToFilter('is_active', 1)
+            ->addFieldToFilter('name', $this->getBlockId());
 
-            $html = "";
+        $html = "";
 
-            foreach ( $agreements as $agreement ) {
-                $html .= $agreement->getContent();
-                break;
-            }
+        foreach ($agreements as $agreement) {
+            $html .= $agreement->getContent();
+            break;
+        }
 
-            return $html; 
-	}
+        return $html;
+    }
 }
-
-
-/* vim: set ts=4 sw=4 expandtab nu tw=90: */
-
