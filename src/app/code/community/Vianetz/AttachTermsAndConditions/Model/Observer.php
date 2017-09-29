f5e9c059cf9761ccc6d9b743fd8aea8b8465271a
Christoph Massmann <c.massmann@vianetz.com>
2017-09-29 19:01:28 +0200

    ATC-4:  - added dependency to Vianetz_Core to be able to log information  - added try-catch around whole pdf attachments process  - refactored some parts to make them more readable  - now the store id of the order is used for admin

--- src/app/code/community/Vianetz/AttachTermsAndConditions/Model/Observer.php
+++ src/app/code/community/Vianetz/AttachTermsAndConditions/Model/Observer.php
@@ -24,35 +24,79 @@ class Vianetz_AttachTermsAndConditions_Model_Observer
     /**
      * Add pdf documents as email attachments.
      *
+     * Event: vianetz_pdfattachments_email_template_init
+     *
      * @param Varien_Event_Observer $observer
      *
      * @return Vianetz_AttachTermsAndConditions_Model_Observer
      */
     public function addPdfAttachment(Varien_Event_Observer $observer)
     {
-        /** @var Mage_Core_Model_Email_Template_Mailer $mailer */
-        $mailer = $observer->getEvent()->getMailer();
+        try {
+            /** @var Mage_Core_Model_Email_Template_Mailer $mailer */
+            $mailer = $observer->getEvent()->getMailer();
 
-        $orderEmailTemplate = Mage::getStoreConfig(Mage_Sales_Model_Order::XML_PATH_EMAIL_TEMPLATE, $mailer->getStoreId());
-        if ($mailer->getTemplateId() != $orderEmailTemplate) {
-            return $this;
-        }
-
-        /** @var Mage_Core_Model_Email_Template $emailTemplate */
-        $emailTemplate = $observer->getEvent()->getEmailTemplate();
-
-        $agreements = Mage::getModel('checkout/agreement')->getCollection()
-            ->addStoreFilter(Mage::app()->getStore()->getId())
-            ->addFieldToFilter('is_active', 1);
-
-        foreach ($agreements as $agreement) {
-            $file = Mage::getBaseDir('media') . DS . $agreement->getName() . '.pdf';
-            if (file_exists($file) === true) {
-                $filename = Mage::helper('sales')->__($agreement->getName()).'.pdf';
-                Mage::helper('pdfattachments')->addAttachmentToEmail($emailTemplate, file_get_contents($file), $filename);
+            $orderEmailTemplate = Mage::getStoreConfig(Mage_Sales_Model_Order::XML_PATH_EMAIL_TEMPLATE, $mailer->getStoreId());
+            if ($mailer->getTemplateId() != $orderEmailTemplate) {
+                return $this;
             }
+
+            /** @var Mage_Core_Model_Email_Template $emailTemplate */
+            $emailTemplate = $observer->getEvent()->getEmailTemplate();
+
+            $agreements = Mage::getModel('checkout/agreement')->getCollection()->addStoreFilter($this->getStoreId())->addFieldToFilter('is_active', 1);
+
+            if (count($agreements) === 0) {
+                $this->getHelper()->log('No active agreements found for store ' . $this->getStoreId() . '.');
+                return $this;
+            }
+
+            foreach ($agreements as $agreement) {
+                $file = Mage::getBaseDir('media') . DS . $agreement->getName() . '.pdf';
+                $this->getHelper()->log('Searching for attachment file ' . $file);
+
+                if (file_exists($file) === true) {
+                    $this->getHelper()->log('Attaching ' . $file . ' to order email for order #' . $this->getOrder()->getIncrementId() . '.');
+
+                    $filename = Mage::helper('sales')->__($agreement->getName()) . '.pdf';
+                    Mage::helper('pdfattachments')->addAttachmentToEmail($emailTemplate, file_get_contents($file), $filename);
+                }
+            }
+        } catch (Exception $exception) {
+            $this->getHelper()->log('Error while attaching pdf document: ' . $exception->getMessage() . ' ' . $exception->getTraceAsString());
         }
 
         return $this;
+    }
+
+    /**
+     * @return integer
+     */
+    private function getStoreId()
+    {
+        $storeId = (int)Mage::app()->getStore()->getId();
+        $order = $this->getOrder();
+
+        if ($storeId === Mage_Core_Model_App::ADMIN_STORE_ID && empty($order) === false) {
+            $storeId = $this->getOrder()->getStoreId();
+        }
+
+        return $storeId;
+    }
+
+    /**
+     * @return Mage_Sales_Model_Order
+     */
+    private function getOrder()
+    {
+        return Mage::registry('current_order');
+    }
+
+    /**
+     * @return \Vianetz_AttachTermsAndConditions_Helper_Data
+     */
+    private function getHelper()
+    {
+        return Mage::helper('attachtermsandconditions');
     }
 }
